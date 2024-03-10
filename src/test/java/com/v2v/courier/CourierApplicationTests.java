3c602d047ec83f8104335cc95e0515aeb7200f49
Santhosh S <officialuse3012@gmail.com>
2024-03-10 12:44:34 +0530

    Initial commit

---
+++ src/test/java/com/v2v/courier/CourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v2v.courier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierApplicationTests {
+
+  @Test
+  void contextLoads(){
+  }
+
+}
